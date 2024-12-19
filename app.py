f4820793b41ff482950f2430d34890e742d25ea3
Analista Radar Octavo <francisco.gordillo@centrohub.co>
2024-12-19 11:59:11 -0500

    dos

--- app.py
+++ app.py
@@ -88,6 +88,6 @@ def guardar_ids():
 
 if __name__ == '__main__':
     # Asegurarse de que Flask use el puerto adecuado en Render (o en cualquier entorno de producción)
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=True)
 
 
